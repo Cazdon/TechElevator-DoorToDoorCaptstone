2cbf59bbc6ff13712fe82d593ce119a1597407eb
zacharyhall <z.leo@outlook.com>
2019-04-08 12:54:16 -0400

    model finished, view mostly finished, working on controller and DAL

--- DoorToDoorWeb/Models/Salesperson.cs
+++ DoorToDoorWeb/Models/Salesperson.cs
@@ -8,6 +8,38 @@ namespace DoorToDoorWeb.Models
 {
     public class Salesperson
     {
+        public int SalespersonID { get; set; }
 
+        [Required(ErrorMessage = "*")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string ConfirmPassword { get; set; }
     }
 }
+
+//- Add Salesperson fields:
+//- - First Name
+//- - - Required
+//- - - Max Length: 50 characters
+//- - Last Name
+//- - - Required
+//- - - Max Length: 50 characters
+//- - Email Address
+//- - - Required
+//- - - Unique
+//- - - Max Length: 100 characters
+//- - Password
+//- - - Required
+//- - Confirm Password
+//- - - Required
+//- - - Must match Password
