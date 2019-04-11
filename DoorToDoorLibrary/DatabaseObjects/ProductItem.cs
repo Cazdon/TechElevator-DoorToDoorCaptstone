8ea982ca2573443e71dfa01499f7312ed4dde656
josephknutson <JKnutson101@gmail.com>
2019-04-11 11:43:56 -0400

    Added Product Item, Sales Trasaction Item, and wireframes

---
+++ DoorToDoorLibrary/DatabaseObjects/ProductItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorToDoorLibrary.Models
+{
+    public class ProductItem : BaseItem
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ DoorToDoorLibrary/DatabaseObjects/SalesTransactionItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorToDoorLibrary.Models
+{
+    public class SalesTransactionItem : BaseItem
+    {
+        public DateTime Date { get; set; }
+        public int HouseID { get; set; }
+        public int ProductID { get; set; }
+        public int SalesmenID { get; set; }
+        public int Amount { get; set; }
+    }
+}
