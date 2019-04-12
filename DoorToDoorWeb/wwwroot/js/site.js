ae8f2e6345cfd64a9b2a135bdf98bbc6e547519c
williamterlep <terlepw@gmail.com>
2019-04-12 13:21:54 -0400

    house lists and details for manager and salesperson

--- DoorToDoorWeb/wwwroot/js/site.js
+++ DoorToDoorWeb/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const formToggleElement = document.getElementById("show-hide-form");
 
-// Write your JavaScript code.
+function toggleForm() {
+    let formElement = document.querySelector("form");
+
+    formElement.toggleAttribute("hidden");
+}
+
+formToggleElement.addEventListener('click', (event) => {
+    toggleForm();
+});
