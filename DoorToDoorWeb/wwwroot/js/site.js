7f86ac350af592a253de4183fd063c3b4e316022
williamterlep <terlepw@gmail.com>
2019-04-12 16:15:50 -0400

    changed show hide from button to not button

--- DoorToDoorWeb/wwwroot/js/site.js
+++ DoorToDoorWeb/wwwroot/js/site.js
@@ -1,11 +1,1 @@
-﻿const formToggleElement = document.getElementById("show-hide-form");
-
-function toggleForm() {
-    let formElement = document.querySelector("form");
-
-    formElement.toggleAttribute("hidden");
-}
-
-formToggleElement.addEventListener('click', (event) => {
-    toggleForm();
-});
+﻿
