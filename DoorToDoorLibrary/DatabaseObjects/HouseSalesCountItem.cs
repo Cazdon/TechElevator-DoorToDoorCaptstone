cee01a0a329573bbc1884624074af43ae8cee590
josephknutson <JKnutson101@gmail.com>
2019-04-15 11:24:45 -0400

    Finished functionality on Manager Dashboard, still needs CSS

--- DoorToDoorLibrary/DatabaseObjects/HouseSalesCountItem.cs
+++ DoorToDoorLibrary/DatabaseObjects/HouseSalesCountItem.cs
@@ -6,6 +6,10 @@ namespace DoorToDoorLibrary.DatabaseObjects
 {
     public class HouseSalesCountItem : BaseItem
     {
-
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Country { get; set; }
+        public int SalesCount { get; set; }
     }
 }
